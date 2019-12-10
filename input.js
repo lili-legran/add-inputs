7da75915d5f74ef3f94773a9acde11f2f34ea4ce
Lili Legran <lili_akhunyanova@icloud.com>
2019-12-10 22:11:50 +0300

    BEM naming

--- input.js
+++ input.js
@@ -1,16 +1,15 @@
 
-let firstRow = document.querySelector('.input__first-row');
-let secondRow = document.querySelector('.input__second-row');
+let firstRow = document.querySelector('.inputs__first');
+let secondRow = document.querySelector('.inputs__second');
 let firstRowValue = 0;
 let secondRowValue = 0;
-let resultRow = document.querySelector('.input__result');
+let resultRow = document.querySelector('.inputs__result');
 
 const sumCounter = () => {
   firstRowValue = +firstRow.value;
   secondRowValue = +secondRow.value;
 
   if (!isNaN(firstRowValue) && !isNaN(secondRowValue)) {
-    let sum = firstRowValue + secondRowValue;
-    resultRow.innerHTML = sum;
+    resultRow.innerHTML = firstRowValue + secondRowValue;
   }
 }
